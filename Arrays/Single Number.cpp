7611ef581a97d11ab6f77f2f408c1823253c64cb
Ali Husnain <122731330+alihusnain88@users.noreply.github.com>
2024-06-24 00:09:04 +0500

    Created Single Number.cpp

---
+++ Arrays/Single Number.cpp
@@ -0,0 +1,23 @@
+// 136.  Single Number
+// Description:
+// Every element of array appears twice, except one. Find that element that occurs once.
+
+// Link:
+// https://leetcode.com/problems/single-number/
+
+// Solution:
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int size = nums.size();
+        sort(nums.begin(), nums.end());
+        int i;
+       
+        for(i=1; i<size; i=i+2){
+            if(nums[i]!=nums[i-1] ){
+                return nums[i-1];
+            }
+        }        
+        return nums[size-1];
+    }
+};
